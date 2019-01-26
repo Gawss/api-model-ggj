e227fae0fc502540c347fc769f0958e68f290b0b
GAWSSODIA\LuisMiguel <luispalacio1997@hotmail.com>
2019-01-26 06:26:15 -0500

    multiplayer & json parser

--- server.py
+++ server.py
@@ -47,25 +47,105 @@ print("Loaded model from disk")
 
 
 app = Flask(__name__)
+
+numPlayers = 0
+startLvl = False
+
+playerOne_posX = 0
+playerOne_posY = 0
+playerTwo_posX = 0
+playerTwo_posY = 0
+playerThree_posX = 0
+playerThree_posY = 0
+playerFour_posX = 0
+playerFour_posY = 0
  
-@app.route("/")
+@app.route("/", methods=['GET', 'PUT', 'POST'])
 def predict():
-    #model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    #model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['mae', 'acc'])
-    loaded_model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae', 'acc'])
+
+    print(request.content_type)
     
     prediction = scaler_3.inverse_transform(loaded_model.predict(inputs_testing, batch_size=10, verbose=0, steps=None))
+
     if(request.get_json()):
         rq_body = request.get_json()
         print(rq_body)
         print("health: " + str(rq_body['health']))
+        
     data = {
         'positionX': '1000',
         'positionY': '500',
         'value': '300',
         'type': 'powerUp_02'
     }
+    print(prediction)
+    return jsonify(data)
+
+@app.route("/start", methods=['POST'])
+def start():
+    global numPlayers
+    global startLvl
+
+    if(request.get_json()['ready'] == 'False'):
+        if(numPlayers < 4):
+            newPlayerID = numPlayers + 1
+            numPlayers = newPlayerID
+            data = {
+                'player_num': newPlayerID,
+                'start': 'False'
+            }
+        else:
+            data = {
+                'start': 'True'
+            }
+            startLvl = True
+    else:
+        if(numPlayers == 4):
+            data = {
+                'start': 'True'
+            }
+        else:
+            data = {
+                'start': 'False'
+            }
+    print(numPlayers)
+    print(startLvl)
+    return jsonify(data)
+
+@app.route("/update", methods=['POST'])
+def update():
+    global playerOne_posX
+    global playerOne_posY
+    global playerTwo_posX
+    global playerTwo_posY
+    global playerThree_posX
+    global playerThree_posY
+    global playerFour_posX
+    global playerFour_posY
     
+    if(request.get_json()['playerID'] == '1'):
+        playerOne_posX = request.get_json()['posX']
+        playerOne_posY = request.get_json()['posY']
+    elif(request.get_json()['playerID'] == '2'):
+        playerTwo_posX = request.get_json()['posX']
+        playerTwo_posY = request.get_json()['posY']
+    elif(request.get_json()['playerID'] == '3'):
+        playerThree_posX = request.get_json()['posX']
+        playerThree_posY = request.get_json()['posY']
+    elif(request.get_json()['playerID'] == '4'):
+        playerFour_posX = request.get_json()['posX']
+        playerFour_posY = request.get_json()['posY']        
+    
+    data = {
+            'playerOne_posX': playerOne_posX,
+            'playerOne_posY': playerOne_posY,
+            'playerTwo_posX': playerTwo_posX,
+            'playerTwo_posY': playerTwo_posY,
+            'playerThree_posX': playerThree_posX,
+            'playerThree_posY': playerThree_posY,
+            'playerFour_posX': playerFour_posX,
+            'playerFour_posY': playerFour_posY,
+        }
     return jsonify(data)
  
 if __name__ == "__main__":
